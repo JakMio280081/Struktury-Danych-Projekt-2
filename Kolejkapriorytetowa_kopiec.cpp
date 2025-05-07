4280943f5ae3fd9cefa8d7212415e07355b44919
xFekon <146870740+xFekon@users.noreply.github.com>
2025-05-07 12:50:42 +0200

    Add files via upload

--- Kolejkapriorytetowa_kopiec.cpp
+++ Kolejkapriorytetowa_kopiec.cpp
@@ -9,7 +9,7 @@ void Kolejkapriorytetowa_kopiec::heapify_up(int index) {
         std::swap(heap[index], heap[parent]);
         index = parent;
     }
-}
+}// Poprawia w³aœciowoœæ kopca gdy dodajemy now¹ wartoœæ powtarza siê dopóki priorytet aktualnego elementu nie jest wiêkszy ni¿ rodzic
 
 void Kolejkapriorytetowa_kopiec::heapify_down(int index) {
     int size = heap.size();
@@ -28,12 +28,12 @@ void Kolejkapriorytetowa_kopiec::heapify_down(int index) {
         std::swap(heap[index], heap[largest]);
         index = largest;
     }
-}
+}// Poprawia w³aœciwoœc kopca gdy usuwamy najwiêkszy element i wykonuje siê dopóki aktualny element jest mniejszy od któregoœ z dzieci
 
 void Kolejkapriorytetowa_kopiec::insert(const std::string& value, int priority) {
     heap.emplace_back(priority, value);
     heapify_up(heap.size() - 1);
-}
+}// dodaje na koniec kopca nowy element jako liœæ
 
 std::string Kolejkapriorytetowa_kopiec::extract_max() {
     if (heap.empty()) throw std::runtime_error("Heap is empty!");
@@ -44,12 +44,12 @@ std::string Kolejkapriorytetowa_kopiec::extract_max() {
     heapify_down(0);
 
     return max_value;
-}
+}// zwraca najwiêksz¹ wartoœæ w kopcu czyli wartoœæ na pozycji 0 po czym zalepia t¹ dziurê ostatnim elementem i wykonuje heapify down
 
 std::string Kolejkapriorytetowa_kopiec::peek() const {
     if (heap.empty()) throw std::runtime_error("Heap is empty!");
     return heap[0].second;
-}
+}// zwraca wartoœæ najwiêkszego elementu kopca, który znajdujê siê na pierwsze pozycji pojemnika
 
 void Kolejkapriorytetowa_kopiec::modify_key(const std::string& value, int new_priority) {
     for (size_t i = 0; i < heap.size(); ++i) {
@@ -62,12 +62,12 @@ void Kolejkapriorytetowa_kopiec::modify_key(const std::string& value, int new_pr
         }
     }
     throw std::runtime_error("Element not found: " + value);
-}
+}// szuka odpowiedniego elemntu i zmienia jego priorytet i zale¿nie od zmiany poprawia kopiec funkcjami heapify
 
 size_t Kolejkapriorytetowa_kopiec::return_size() const {
     return heap.size();
-}
+}// zwraca rozmiar stryktury w czasie O(1)
 
 void Kolejkapriorytetowa_kopiec::clear() {
     heap.clear();
-}
+}// usuwa ca³¹ zawartoœæ struktury
--- Struk_Dan_P2.cpp
+++ Struk_Dan_P2.cpp
@@ -12,7 +12,6 @@
 using namespace std;
 using namespace chrono;
 
-// Wczytuje 10 zbiorów danych z pliku
 vector<vector<pair<string, int>>> wczytaj_dane(const string& nazwa_pliku, int rozmiar) {
     ifstream plik(nazwa_pliku);
     vector<vector<pair<string, int>>> zbiory;
@@ -33,10 +32,10 @@ vector<vector<pair<string, int>>> wczytaj_dane(const string& nazwa_pliku, int ro
             ss >> nazwa >> priorytet;
             aktualny_zbior.emplace_back(nazwa, priorytet);
         }
-    }
+    }// pobiera dane z pliku i zapisuje je w zbiorach w tablicy
 
     if (!aktualny_zbior.empty()) {
-        zbiory.push_back(aktualny_zbior); // dodaj ostatni zestaw
+        zbiory.push_back(aktualny_zbior); // dodaje ostatni zestaw
     }
 
     return zbiory;
@@ -44,7 +43,7 @@ vector<vector<pair<string, int>>> wczytaj_dane(const string& nazwa_pliku, int ro
 
 void test() {
     vector<int> rozmiary = { 5000, 8000, 10000, 16000, 20000, 40000, 60000, 100000 };
-    string operacja = "insert_kp_tablica";
+    string operacja = "extract_max_altenative_kp_kopiec";
     ofstream csv("wyniki_" + operacja + ".csv");
     csv << "rozmiar,czas_usredniony_ns\n";
 
@@ -54,9 +53,9 @@ void test() {
 
         long long suma_czasu = 0;
 
-        for (const auto& zbior : zbiory) {
-            for (int i = 0; i < 100; ++i) {
-                PriorityQueueArray pq;
+        for (const auto& zbior : zbiory) { //operujemy na jednym z 10 zbiorów danych dla danego rozmiaru
+            for (int i = 0; i < 100; ++i) { // powtarzamy operacje 100 razy dla tego samego zbioru dla lepszego uśrednienia wyników
+                Kolejkapriorytetowa_kopiec pq;
                 for (int j = 0; j < zbior.size() - 1; ++j) {
                     pq.insert(zbior[j].first, zbior[j].second);
                 }
@@ -64,20 +63,20 @@ void test() {
                 
 
                 auto start = high_resolution_clock::now();
+                //for (int i = 0; i < 100; ++i) {
+                    pq.extract_max(); //wykonywana operacja
+                //}
+                    auto end = high_resolution_clock::now();
                 
-                    pq.insert("new", 123456);
-           
-                auto end = high_resolution_clock::now();
-
                 auto czas = duration_cast<nanoseconds>(end - start).count();
                 suma_czasu += czas;
             }
         }
 
-        double sredni_czas = static_cast<double>(suma_czasu) / (zbiory.size() * 100);
+        double sredni_czas = static_cast<double>(suma_czasu) / (zbiory.size() * 100);// uśrednienie czasów
         csv << rozmiar << "," << sredni_czas << "\n";
         cout << "Zakonczono: " << rozmiar << " - średni czas: " << sredni_czas << " ns\n";
-    }
+    }// zapisuje do pliku średnie wyniki danej operacji dla każdego rozmiaru struktury (operacja zmieniana jest ręcznie dla każdego testu, wraz z nazwą pliku do którego zostanie zapisana)
 
     csv.close();
 }
@@ -86,7 +85,7 @@ void test() {
 
 int main()
 {
-    test();
+    test(); // wykonujemy zadany wyżej test
 }
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
 // Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
